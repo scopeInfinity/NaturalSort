f5e4e426cb1419b36eaf91555060c78ffd80da05
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-02-08 20:21:25 +0000

    Added basic tests

--- natural_sort_test.cpp
+++ natural_sort_test.cpp
@@ -1,42 +1,95 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <assert.h>
 #include "natural_sort.hpp"
 
+#define ADD_TEST(test_method) { \
+    std::cout<<"Running test " #test_method "..."<<std::endl; \
+    test_method(); \
+    std::cout<<"Test " #test_method " is successful."<<std::endl; \
+}
+
+#define ASSERT_TRUE(x) { assert(x); }
+#define ASSERT_FALSE(x) { assert(!(x)); }
+#define ASSERT_EQ(x, y) { assert((x) == (y)); }
+
+void test_compare() {
+    ASSERT_FALSE(SI::natural::compare<std::string>("Hello 32", "Hello 023"));
+    ASSERT_FALSE(SI::natural::compare<std::string>("Hello 32a", "Hello 32"));
+    ASSERT_TRUE(SI::natural::compare<std::string>("Hello 32", "Hello 32a"));
+    ASSERT_FALSE(SI::natural::compare<std::string>("Hello 32.1", "Hello 32"));
+    ASSERT_TRUE(SI::natural::compare<std::string>("Hello 32", "Hello 32.1"));
+    ASSERT_FALSE(SI::natural::compare<std::string>("Hello 32", "Hello 32"));
+}
+
+void test_sort_vector() {
+    std::vector<std::string> data = {
+        "Hello 100",
+        "Hello 34",
+        "Hello 9",
+        "Hello 25",
+        "Hello 10",
+        "Hello 8",
+    };
+    std::vector<std::string> want = {
+        "Hello 8",
+        "Hello 9",
+        "Hello 10",
+        "Hello 25",
+        "Hello 34",
+        "Hello 100",
+    };
+    SI::natural::sort(data);
+    ASSERT_EQ(data, want);
+}
+
+void test_sort_array() {
+    const int SZ = 3;
+    std::string data[SZ] = {
+        "Hello 100",
+        "Hello 25",
+        "Hello 34",
+    };
+    std::string want[SZ] = {
+        "Hello 25",
+        "Hello 34",
+        "Hello 100",
+    };
+
+    SI::natural::sort<std::string, SZ>(data);
+    for(int i=0; i<SZ; i++) {
+        ASSERT_EQ(data[i], want[i]);
+    }
+}
+
+void test_sort_float() {
+    std::vector<std::string> data = {
+        "Hello 1",
+        "Hello 10",
+        "Hello 10.3",
+        "Hello 2",
+        "Hello 10.23",
+        "Hello 10.230",
+    };
+    std::vector<std::string> want = {
+        "Hello 1",
+        "Hello 2",
+        "Hello 10",
+        "Hello 10.23",
+        "Hello 10.230",
+        "Hello 10.3",
+    };
+
+    SI::natural::sort(data);
+    ASSERT_EQ(data, want);
+}
+
 int main()
-{	
-	std::vector<std::string> v;
-	std::string s;
-	std::cout<<"Comparision of \"Hello 32\" and \"Hello 023\" : "<<(SI::natural::compare<std::string>("Hello 32","Hello 023"))<<" (Expected: 0)"<<std::endl;
-	std::cout<<"Comparision of \"Hello 32a\" and \"Hello 32\" : "<<(SI::natural::compare<std::string>("Hello 32a","Hello 32"))<<" (Expected: 0)"<<std::endl;
-	std::cout<<"Comparision of \"Hello 32\" and \"Hello 32a\" : "<<(SI::natural::compare<std::string>("Hello 32","Hello 32a"))<<" (Expected: 1)"<<std::endl;
-	std::cout<<"Comparision of \"Hello 32.1\" and \"Hello 32\" : "<<(SI::natural::compare<std::string>("Hello 32.1","Hello 32"))<<" (Expected: 0)"<<std::endl;
-	std::cout<<"Comparision of \"Hello 32\" and \"Hello 32.1\" : "<<(SI::natural::compare<std::string>("Hello 32","Hello 32.1"))<<" (Expected: 1)"<<std::endl;
-	std::cout<<"Comparision of \"Hello 32\" and \"Hello 32\" : "<<(SI::natural::compare<std::string>("Hello 32","Hello 32"))<<" (Expected: 0)"<<std::endl;
-	while(getline(std::cin,s))
-		v.push_back(s);
-	std::vector<char *> v2;
-	for (std::vector<std::string>::iterator it = v.begin(); it!=v.end(); ++it)
-		v2.push_back((char*)it->c_str());
-	
-
-	SI::natural::sort(v);
-	std::cout<<"Output Using std::string\n";
-	for (std::vector<std::string>::iterator it = v.begin(); it!=v.end(); ++it)
-		std::cout<<*it<<std::endl;
-	
-
-	SI::natural::sort(v2);
-	std::cout<<"Output Using char*\n";
-	for (std::vector<char*>::iterator it = v2.begin(); it!=v2.end(); ++it)
-		std::cout<<*it<<std::endl;
-
-
-	std::cout<<"Output Using array As Container\n";
-	std::string array[5]={"Hello 1","Hello 2","Hello 10","Hello 10.3","Hello 10.23"};
-	SI::natural::sort<std::string,5>(array);
-
-	for(int i=0;i<5;i++)
-		std::cout<<array[i]<<std::endl;
-	return 0;
+{
+    ADD_TEST(test_compare);
+    ADD_TEST(test_sort_vector);
+    ADD_TEST(test_sort_array);
+    ADD_TEST(test_sort_float);
+    return 0;
 }
